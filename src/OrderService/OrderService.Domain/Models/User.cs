b3f48ebc684c2443186203e34182ac4de60116c2
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-05-07 01:22:48 +0300

    update user model

--- src/OrderService/OrderService.Domain/Models/User.cs
+++ src/OrderService/OrderService.Domain/Models/User.cs
@@ -5,10 +5,10 @@ namespace OrderService.Domain.Models
     public class User
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Role { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string Role { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? LastLoginAt { get; set; }
     }
