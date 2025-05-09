1caaf551a781b54b45a3dc8f68a0a72b218bc37e
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-05-09 17:14:06 +0300

    user repo

---
+++ src/UserMicroservice/UserMicroservice.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Domain.Interfaces;
+using UserMicroservice.Domain.Models;
+using UserMicroservice.Infrastructure.Data;
+
+namespace UserMicroservice.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly UserDbContext _context;
+
+    public UserRepository(UserDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _context.Users
+            .Include(u => u.Addresses)
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users
+            .Include(u => u.Addresses)
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(User user)
+    {
+        _context.Users.Remove(user);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+} 
