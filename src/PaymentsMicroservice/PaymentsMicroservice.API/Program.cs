62c71103118bbd7c998ea22ad2a6a0bf8985321d
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-05-12 02:01:07 +0300

    added program.cs

---
+++ src/PaymentsMicroservice/PaymentsMicroservice.API/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentsMicroservice.Application.Interfaces;
+using PaymentsMicroservice.Application.Services;
+using PaymentsMicroservice.Infrastructure.Data;
+using PaymentsMicroservice.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<PaymentsDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddSingleton<IMessageBroker, RabbitMQBroker>();
+builder.Services.AddScoped<IMessageHandler<PaymentsMicroservice.Contracts.Messages.OrderCreatedEvent>, OrderCreatedHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// Subscribe to messages
+var messageBroker = app.Services.GetRequiredService<IMessageBroker>();
+
+// Create a scope for subscription
+using (var scope = app.Services.CreateScope())
+{
+    var orderCreatedHandler = scope.ServiceProvider.GetRequiredService<IMessageHandler<PaymentsMicroservice.Contracts.Messages.OrderCreatedEvent>>();
+    await messageBroker.SubscribeAsync("payments.order.created", "order.created", orderCreatedHandler);
+}
+
+app.Run();
